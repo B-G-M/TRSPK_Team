a587b06a0994fab3ce9bdd41a758523ea26f211d
B-G-M <bgm99@mail.ru>
2021-09-14 20:51:31 +0300

    task_1.2

---
+++ trspk/trspk/task_1/task_1.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace task1_2
+{
+	class Human
+	{
+		public Human(string name, string surname)
+		{
+			this.surname = surname;//Поля для чтения можно инициализировать в конструкторе
+			//this.name = name; Константы должны быть инициализированны при определении
+		}
+		const string name = "Василий";
+		readonly string surname = "Тёмный";
+		private int age = 20;
+
+		public string Name
+		{
+			get { return name; }
+			//set { myVar = value; }
+		}
+		public string Surname
+		{
+			get { return surname; }
+			//set { surname = value; } - Поля разрешенные только
+			//для чтения можно инициализировать при их объявлении
+			//либо на уровне класса, либо инициилизировать и изменять
+			//в конструкторе. Инициализировать или изменять их значение
+			//в других местах нельзя. 
+		}
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+
+	}
+
+
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Human H = new Human("Валерий","Соколов");
+			Console.WriteLine("Age: " + H.Age);
+			Console.WriteLine("Surname: " + H.Surname);
+			H.Age = 31;
+			Console.WriteLine("Age: " + H.Age);
+		}
+	}
+}
