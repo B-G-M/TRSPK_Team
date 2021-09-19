27135f738abb85377082b0fe040d45f1b5f708ab
B-G-M <bgm99@mail.ru>
2021-09-20 01:19:51 +0300

    task_2.2upd

--- trspk/task_2/Ебанутые_идеи_Саввы/Program.cs
+++ trspk/task_2/Ебанутые_идеи_Саввы/Program.cs
@@ -30,11 +30,6 @@ namespace task2_2
 				num1_lenght = number1.number.Length,
 				num2_lenght = number2.number.Length;
 
-			if (number1.number[0] == '-' && number2.number[0] != '-' || number1.number[0] != '-' && number2.number[0] == '-')
-			{
-				return number1 - number2;// хуета с минусом, хуй знает как это решить
-			}
-
 			if (num1_lenght > num2_lenght)
 			{
 				sum.number = number1.number;
@@ -43,12 +38,12 @@ namespace task2_2
 				sum.number = "";
 			}
 
-			for (	; num1_lenght > 0; num1_lenght--, num2_lenght--)// пизда+говно+моча (1 символ проебали мейби)
+			for (; num1_lenght > 0; num1_lenght--, num2_lenght--)// пизда+говно+моча (1 символ проебали мейби)
 			{
 				temp1 += Convert.ToInt32(number1.number[num1_lenght]);
 				temp2 += Convert.ToInt32(number2.number[num2_lenght]);
 
-				if(temp1 + temp2 < 9) 
+				if (temp1 + temp2 < 9)
 				{
 					sum.number = sum.number.Insert(0, Convert.ToString(temp1 + temp2));
 					continue;
@@ -58,16 +53,12 @@ namespace task2_2
 				temp2 = 1;
 				sum.number = sum.number.Insert(0, Convert.ToString(temp1));
 				temp1 = 0;
-
-				if (number1.number[num1_lenght - 1] == '-' || number2.number[num1_lenght - 1] == '-'){break;}
 			}
 
 			for (; 0 < num2_lenght; num2_lenght--)
 			{
-				temp2 = Convert.ToInt32(number2.number[num2_lenght]);
+				temp2 += Convert.ToInt32(number2.number[num2_lenght]);
 				sum.number = sum.number.Insert(0, Convert.ToString(temp2));
-
-				if (number2.number[num1_lenght - 1] == '-') { break; }
 			}
 
 			if (number1.number[0] == '-' && number2.number[0] == '-')
@@ -78,27 +69,19 @@ namespace task2_2
 		}
 		public static LongNumber operator -(LongNumber number1, LongNumber number2)
 		{
-			//Сделать вычитание из меньшего большего
-			// Закоментируй код сука !!!!!!
-			// Закоментируй код сука !!!!!!
-			// Закоментируй код сука !!!!!!
-			// Закоментируй код сука !!!!!!
-			// Закоментируй код сука !!!!!!
-			// Закоментируй код сука !!!!!!
-
 			LongNumber sum = new();
-			int temp1,
-				temp2,
+			int temp1 = 0,
+				temp2 = 0,
 				num1_lenght = number1.number.Length,
 				num2_lenght = number2.number.Length,
 				i = num1_lenght < num2_lenght ? num1_lenght : num2_lenght;
 
-			for ( ;i > 0; i--)
+			for (; i > 0; i--)
 			{
 				temp1 = Convert.ToInt32(number1.number[num1_lenght]);
 				temp2 = Convert.ToInt32(number2.number[num2_lenght]);
 
-				if( temp1 - temp2 > 0)
+				if (temp1 - temp2 > 0)
 				{
 					sum.number = sum.number.Insert(0, Convert.ToString(temp1 - temp2));
 					num1_lenght--;
@@ -112,93 +95,52 @@ namespace task2_2
 
 				num1_lenght--;
 				num2_lenght--;
-
-				if (number1.number[num1_lenght] == '-' || number2.number[num2_lenght] == '-'){break;}
-			}
-
-			for (; 0 < num1_lenght; num1_lenght--)
-			{
-				temp1 = Convert.ToInt32(number1.number[num1_lenght]);
-				sum.number = sum.number.Insert(0, Convert.ToString(temp1));
-
-				if (number1.number[num1_lenght - 1] == '-') { break; }
-			}
-
-			if (number1.number[0] == '-' && number2.number[0] == '-')
-			{
-				sum.number = sum.number.Insert(0, Convert.ToString('-'));
 			}
 			return sum;
 		}
 		public static bool operator <(LongNumber number1, LongNumber number2)
 		{
-			bool da = true,
-				positive = true;
-			int i = 0,
-				temp1,
+			bool da = true;
+			int temp1,
 				temp2;
-
-			if (number1.number[0] == '-' && number2.number[0] != '-')
-			{
-				return da;
-			}
-
-			if (number2.number[0] == '-' && number1.number[0] != '-')
-			{
-				return !da;
-			}
-
-			if (number2.number[0] == '-' && number1.number[0] == '-')
-			{
-				positive = !positive;
-				i++;
-			}
 
 			if (number1.number.Length < number2.number.Length)
 			{
-				if (positive)
-				{
-					return da;
-				}
-				return !da;
-			}
-			else if(number1.number.Length > number2.number.Length)
-			{
-				if (positive)
-				{
-					return !da;
-				}
+				da = false;
 				return da;
 			}
-
-			for (	; i < number1.number.Length; i++)
+			else if (number1.number.Length > number2.number.Length)
+			{
+				return da;
+			}
+			for (int i = 0; i < number1.number.Length; i++)
 			{
 				temp1 = Convert.ToInt32(number1.number[i]);
 				temp2 = Convert.ToInt32(number2.number[i]);
 
 				if (temp1 < temp2)
 				{
-					if (positive)
-					{
-						return da;
-					}
-					return !da;
+					da = false;
+					return da;
 				}
 			}
 			return da;
 		}
 		public static bool operator >(LongNumber number1, LongNumber number2)
 		{
-			return !(number1 < number2);
+			bool da = true;
+
+
+			return da;
 		}
-		private static void Give_ten(ref LongNumber number,int i)
+		private static void Give_ten(ref LongNumber number, int i)
 		{
 			char[] arr = number.number.ToCharArray();
 			int k = i - 1,
 				temp;
 			while (true)
 			{
-				if(arr[k] != '0')
+				if (arr[k] != '0')
 				{
 					temp = Convert.ToInt32(arr[k]);
 					temp--;
@@ -208,7 +150,7 @@ namespace task2_2
 				}
 				k--;
 			}
-			while(k != i - 1)//возможно нужны скобки
+			while (k != i - 1)//возможно нужны скобки
 			{
 				temp = 9;
 				arr[k] = Convert.ToChar(temp);
