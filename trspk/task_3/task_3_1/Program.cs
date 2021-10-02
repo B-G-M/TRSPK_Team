a3bc50b1c835f9fe894b0f96f2a79f22c094d44c
B-G-M <bgm99@mail.ru>
2021-10-02 14:55:37 +0300

    task_3.1

--- trspk/task_3/task_3_1/Program.cs
+++ trspk/task_3/task_3_1/Program.cs
@@ -6,14 +6,48 @@ namespace task_3_1
 	{
 		static void Main(string[] args)
 		{
-			
+			int a, b, up, down;
+			a = Convert.ToInt32(Console.ReadLine()); // кол-во измерений (cтолбцов)
+			b = Convert.ToInt32(Console.ReadLine()); // кол-во измерений (строк)
+			Console.WriteLine("Введите нижнюю границу: ");
+			down = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Введите верхнюю границу: ");
+			up = Convert.ToInt32(Console.ReadLine());
+
+			if (down > up)
+			{
+				Console.WriteLine("Нижняя граница больше верхней ");
+				int temp = 0;
+				temp = down;
+				down = up;
+				up = temp;
+			}
+
+			int[,] myArr = new int[a, b];
+			Random random = new Random();
+
+			for (int i = 0; i < a; i++)
+			{
+				for (int j = 0; j < b; j++)
+				{
+					myArr[i, j] = random.Next(down, up);
+				}
+			}
+
+			Console.WriteLine("Кол-во столбцов: " + a);
+			Console.WriteLine("Кол-во строк: " + b);
+			Console.WriteLine("Нижняя граница: " + down);
+			Console.WriteLine("Верхняя граница: " + up);
 
 
-
-
-
-
-
+			for (int i = 0; i < a; i++)
+			{
+				for (int j = 0; j < b; j++)
+				{
+					Console.Write(myArr[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
