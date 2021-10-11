f73cc4065066118ac84cd12e622d1e688f1ed1d4
outplayedru <malofeevs00@gmail.com>
2021-10-11 15:37:53 +0300

    0 punkt

--- trspk/task_5/task_5/Program.cs
+++ trspk/task_5/task_5/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace task_5
+namespace task5
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ : ");
+            string str1 = Console.ReadLine();
+            char ch1 = Convert.ToChar(str1);
+            Console.WriteLine(Char.GetUnicodeCategory(ch1));
+        }
+    }
 }
