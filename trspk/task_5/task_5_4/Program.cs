bd1df76056ab38f3baf0717947b014e7219358c9
B-G-M <bgm99@mail.ru>
2021-10-12 21:38:48 +0300

    task_5.4 done

--- trspk/task_5/task_5_4/Program.cs
+++ trspk/task_5/task_5_4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace task_5_4
 {
@@ -6,7 +7,43 @@ namespace task_5_4
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			DateTime start = DateTime.Now;
+			StringBuilder sbHello = new("Hello");
+			StringBuilder sbWorld = new("World!");
+			StringBuilder sbHW = new();
+			sbHW.Append(sbHello);
+			sbHW.Append(sbWorld);
+			DateTime end = DateTime.Now;
+			Console.WriteLine("\nSbHW: {0}", sbHW);
+			Console.WriteLine("Time Sb: {0}\n", end - start);
+
+			start = DateTime.Now;
+			string strHello = "Hello";
+			string strWorld = "World!";
+			string strHW = strHello + strWorld;
+			end = DateTime.Now;
+			Console.WriteLine("StrHW: {0}", strHW);
+			Console.WriteLine("Time Str: {0}\n", end - start);
+
+			start = DateTime.Now;
+			StringBuilder sbFor = new();
+			for (int i = 0; i < 10000; i++)
+			{
+				sbFor.Append(i);
+			}
+			end = DateTime.Now;
+			//Console.WriteLine("SbFor: {0}", sbFor);
+			Console.WriteLine("Time SbFor: {0}\n", end - start);
+
+			start = DateTime.Now;
+			string strFor = "";
+			for (int i = 0; i < 10000; i++)
+			{
+				strFor += i;
+			}
+			end = DateTime.Now;
+			//Console.WriteLine("Str: {0}", strFor);
+			Console.WriteLine("Time StrFor: {0}\n", end - start);
 		}
 	}
 }
