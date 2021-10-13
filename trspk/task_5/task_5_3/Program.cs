b8169d5dd243696c8de0daa8389335c19ce9c6ac
outplayedru <malofeevs00@gmail.com>
2021-10-13 05:29:22 +0300

    фав

--- trspk/task_5/task_5_3/Program.cs
+++ trspk/task_5/task_5_3/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Text;
+using System.IO;
 
-namespace task_5_3
+namespace _5_3
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите исходный текст : ");
+            string str = Console.ReadLine();
+            
+            Encoding eUTF8 = Encoding.UTF8;
+            Byte[] arr = eUTF8.GetBytes(str);
+
+            Encoding eUTF32 = Encoding.UTF32;
+            Byte[] arr1 = eUTF32.GetBytes(str);
+
+            Encoding ascii = Encoding.ASCII;
+            Byte[] arr2 = ascii.GetBytes(str);
+
+            string path = @"C:\Users\user\Desktop\1.txt";
+            
+            using (StreamWriter fl = new StreamWriter(path))
+            {
+                Console.WriteLine("Исходный текст : ");
+                fl.WriteLine(str);
+
+                fl.Write("UTF-8 : ");
+                foreach (Byte b in arr)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+                fl.Write("UTF-32 : ");
+                foreach (Byte b in arr1)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+                fl.Write("ASCII : ");
+                foreach (Byte b in arr2)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+            }
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string filestr;
+                while ((filestr = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(filestr);
+                }
+            }
+        }
+    }
 }
