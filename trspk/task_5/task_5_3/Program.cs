b0187550095980c734aee10f6f30e3975c0e6884
B-G-M <bgm99@mail.ru>
2021-10-13 10:44:41 +0300

    task_5.3

--- trspk/task_5/task_5_3/Program.cs
+++ trspk/task_5/task_5_3/Program.cs
@@ -2,11 +2,76 @@
 
 namespace task_5_3
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите исходный текст : ");
+            string str = Console.ReadLine();
+
+
+
+            Encoding eUTF8 = Encoding.UTF8;
+            Byte[] arr = eUTF8.GetBytes(str);
+
+            Encoding eUTF32 = Encoding.UTF32;
+            Byte[] arr1 = eUTF32.GetBytes(str);
+
+            Encoding ascii = Encoding.ASCII;
+            Byte[] arr2 = ascii.GetBytes(str);
+
+            Encoding iso = Encoding.GetEncoding(28591); //ISO-8859-1
+            Byte[] arr3 = iso.GetBytes(str);
+
+            Encoding unicode = Encoding.Unicode;
+            Byte[] arr4 = iso.GetBytes(str);
+
+
+            string path = @"C:\Users\Савва\Desktop\1.txt";
+
+            using (StreamWriter fl = new StreamWriter(path))
+            {
+                Console.WriteLine("Исходный текст : ");
+                fl.WriteLine(str);
+
+                fl.Write("UTF-8 : ");
+                foreach (Byte b in arr)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+                fl.Write("UTF-32 : ");
+                foreach (Byte b in arr1)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+                fl.Write("ASCII : ");
+                foreach (Byte b in arr2)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+                fl.Write("ISO-8859-1 : ");
+                foreach (Byte b in arr3)
+                {
+                    fl.Write("[{0}]", b);
+                }
+                fl.WriteLine("\n");
+
+            }
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string filestr;
+                while ((filestr = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(filestr);
+                }
+            }
+        }
+    }
 }
