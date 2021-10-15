c54f9302fabed1f0a58b3d4ce26b99ac7a1cdc59
outplayedru <malofeevs00@gmail.com>
2021-10-15 13:50:15 +0300

    OG go HOME!!!

--- trspk/task_6/task_6_3/Program.cs
+++ trspk/task_6/task_6_3/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+
 namespace task_6_3
 {
 	class Program
 	{
+		enum DayOfWeek
+        {
+			Monday = 1,
+			Tuesday,
+			Wednesday,
+			Thursday,
+			Friday,
+			Saturday,
+			Sunday
+        }
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			DayOfWeek dayOfWeek;
+            Console.WriteLine("Введите число от 1 до 7 : ");
+			int value = Convert.ToInt32(Console.ReadLine());
+
+			if (Enum.IsDefined(typeof(DayOfWeek), value))
+			{
+				dayOfWeek = (DayOfWeek)value;
+            }
+			else
+            {
+				throw new Exception("Некорректный ввод");
+            }
+            Console.WriteLine(dayOfWeek);
 		}
 	}
 }
