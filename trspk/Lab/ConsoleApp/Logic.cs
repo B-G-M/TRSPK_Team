9835a3687b21994984d2fc87c3141589fd974e94
B-G-M <bgm99@mail.ru>
2021-10-22 21:50:51 +0300

    Lab upd

--- trspk/Lab/ConsoleApp/Logic.cs
+++ trspk/Lab/ConsoleApp/Logic.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+
+
 	class Logic
 	{
 		List<Product> products = Product.GetProducts(ReadBD.ProductList());
 		List<Client> clients = Client.GetClient(ReadBD.ClientList());
 
-		double NeedToPay()
+		double NeedToPay(DateTime date)
 		{
 			double needPay = 0;
 
@@ -19,14 +21,71 @@ namespace ConsoleApp
 			{
 				foreach (var product in products)
 				{
-					if (product.KindContribution == client.TypeContribution)
+					if (client.Date.AddMonths(client.Term) < date)
 					{
-
+						continue;
+					}
+					if (product.KindContribution == client.TypeContribution || product.Capital == 0)
+					{
+						needPay += client.Sum * product.Percent / 100 / 12;
+						break;
 					}
 				}
-				client.TypeContribution
 			}
 			return needPay;
 		}
+
+		double[] VolumeOfDeposits()
+		{
+			int count = 0;
+			double[] apm = new double[12];
+			int[] countDep = new int[12];
+			for (int i = 0; i < 12; i++)
+			{
+				foreach (var client in clients)
+				{
+					if (client.Date.Month == i+1)
+					{
+						apm[i] += client.Sum;
+						countDep[i]++;
+					}
+
+				}
+			}
+
+			for (int i = 0; i < 12; i++)
+			{
+				apm[i] /= countDep[i];
+			}
+			
+			return apm;
+		}
+
+		List<double> ProductsProfit()
+		{
+			List<double> profit = new();
+			double temp;
+			foreach (var client in clients)
+			{
+				foreach (var product in products)
+				{
+					if (product.KindContribution == client.TypeContribution)
+					{
+						if (product.Capital == 0)
+						{
+							profit.Add(client.Sum + 
+							           (client.Sum * product.Percent / 12 * client.Term));
+							continue;
+						}
+						profit.Add(client.Sum * Math.Pow((1 + product.Percent 
+							* client.Term * 30 / 365 / 100), product.Capital));
+
+					}
+				}
+			}
+
+			return profit;
+		}
+
 	}
 }
