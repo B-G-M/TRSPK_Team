bc93837598fe6c6f0f0f3d9105c468781f71d500
B-G-M <bgm99@mail.ru>
2021-12-25 22:36:39 +0300

    upd

--- trspk_1/Course_work/Assets/WorldCreator.cs
+++ trspk_1/Course_work/Assets/WorldCreator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WorldCreator : MonoBehaviour
 {
 	public GameObject terra;
-	private static int count = 3;
+	private static int count = 9;
 
 	private void Start()
 	{
